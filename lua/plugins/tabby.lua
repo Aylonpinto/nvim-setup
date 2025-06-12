2389ec7219250e2259ff26961f87973630892245
Aylon Pinto <aylonpinto@MacBook-Pro-van-Aylon.local>
2025-06-12 11:04:16 +0200

    Style tabby

--- lua/plugins/tabby.lua
+++ lua/plugins/tabby.lua
@@ -1,5 +1,16 @@
 return function()
     require('tabby').setup({
-        tabline = require('tabby.presets').active_wins_at_tail,
+        preset = 'active_wins_at_tail',
+        option = {
+            nerdfont = true,
+            theme = {
+                fill = { bg = '#192330' },
+                head = { fg = '#719cd6', bg = '#131a24' },
+                current_tab = { fg = '#dfdfe0', bg = '#63cdcf' },
+                tab = { fg = '#738699', bg = '#1e2a3a' },
+                win = { fg = '#738699', bg = '#1e2a3a' },
+                tail = { fg = '#719cd6', bg = '#131a24' },
+            },
+        },
     })
 end
